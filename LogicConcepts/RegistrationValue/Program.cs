1181efe6c9fbed4091be7ef75d5c5b9a556ed5e6
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-01-17 16:33:03 -0500

    Exercise 08 Completed

--- LogicConcepts/RegistrationValue/Program.cs
+++ LogicConcepts/RegistrationValue/Program.cs
@@ -2,7 +2,9 @@
 
 var answer = string.Empty;
 var options = new List<string> { "s", "n" };
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
 Console.WriteLine("::::: EJERCICIO DE VALOR DE MATRÍCULA :::::");
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
 do
 {
     try
@@ -24,7 +26,7 @@ do
 
     do
     {
-        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?: ", options);
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?.....: ", options);
     } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
 } while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
 Console.WriteLine("::::: GAME OVER :::::");
