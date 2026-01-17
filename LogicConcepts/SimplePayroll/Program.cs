316ec1498534266636ad03f9125f383c857700c3
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-01-17 13:30:35 -0500

    Exercise 06 Completed

---
+++ LogicConcepts/SimplePayroll/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+Console.WriteLine("::::: EJERCICIO DE NÓMINA SENCILLA :::::");
+do
+{
+    try
+    {
+        var name = ConsoleExtension.GetString("Ingrese el nombre......................: ");
+        var workHours = ConsoleExtension.GetFloat("Ingrese número de horas trabajadas.....: ");
+        var hourValue = ConsoleExtension.GetDecimal("Ingrese valor de la hora...............: ");
+        var minimumSalary = ConsoleExtension.GetDecimal("Ingrese valor salario mínimo mensual...: ");
+
+        var salary = (decimal)workHours * hourValue;
+        if (salary < minimumSalary)
+        {
+            Console.WriteLine($"Nombre.................................: {name}");
+            Console.WriteLine($"Salario................................: {minimumSalary:C2}");
+        }
+        else
+        {
+            Console.WriteLine($"Nombre.................................: {name}");
+            Console.WriteLine($"Salario................................: {salary:C2}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
