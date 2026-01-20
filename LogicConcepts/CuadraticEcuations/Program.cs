e3d3e8a233d75a0b6d080fa6720ec17ab9495e5b
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-01-19 19:13:45 -0500

    Exercise 16 Completed

---
+++ LogicConcepts/CuadraticEcuations/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+Console.WriteLine(":::::::: EJERCICIO SERIE DE TAYLOR ::::::::");
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+do
+{
+    try
+    {
+        var a = ConsoleExtension.GetDouble("Ingrese el valor de a: ");
+        var b = ConsoleExtension.GetDouble("Ingrese el valor de b: ");
+        var c = ConsoleExtension.GetDouble("Ingrese el valor de c: ");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?.....: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
+
+public class
