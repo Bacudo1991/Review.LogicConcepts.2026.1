d7160c9801381484e69ce57bc9c5c77d76a39c01
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-01-21 17:45:32 -0500

    Exercise 27 in process

---
+++ LogicConcepts/PalindromePhrases/Program.cs
@@ -0,0 +1,34 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    try
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::");
+        Console.WriteLine("::::::::: EJERCICIO DE FRASES PALINDROMAS :::::::::");
+        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::");
+        var n = ConsoleExtension.GetInt("Cuantas posiciones quieres en el arreglo: ");
+
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?..................: ", options);
+        Console.WriteLine();
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
